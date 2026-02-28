91a71b09bef62f207814be2a4b08e12448285be6
sfluegel <simon.fluegel@uni-osnabrueck.de>
2026-02-28 22:44:46 +0100

    add get_hierarchy_subgraph to init

--- chebi_utils/__init__.py
+++ chebi_utils/__init__.py
@@ -1,6 +1,6 @@
 from chebi_utils.dataset_builder import build_labeled_dataset
 from chebi_utils.downloader import download_chebi_obo, download_chebi_sdf
-from chebi_utils.obo_extractor import build_chebi_graph
+from chebi_utils.obo_extractor import build_chebi_graph, get_hierarchy_subgraph
 from chebi_utils.sdf_extractor import extract_molecules
 from chebi_utils.splitter import create_multilabel_splits
 
@@ -9,6 +9,7 @@ __all__ = [
     "download_chebi_obo",
     "download_chebi_sdf",
     "build_chebi_graph",
+    "get_hierarchy_subgraph",
     "extract_molecules",
     "create_multilabel_splits",
 ]
